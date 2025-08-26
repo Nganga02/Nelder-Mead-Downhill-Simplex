5295ef2f33ded03d64880bf4c0803a1944f61ed8
Nganga02 <nganganicholas50@gmail.com>
2025-08-26 19:34:16 +0300

    Completed the nelder mead function

--- src/nelder-mead.c
+++ src/nelder-mead.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "xmalloc.h"
 #include "nelder-mead.h"
 
@@ -49,7 +51,7 @@ static inline void transform(double *P, double *Q, int n, double beta, double *R
 {
     for (int i = 0; i < n; i++)
     {
-        R[i] =  (P[i] * (1 - beta) + Q[i] * beta);
+        R[i] = (P[i] * (1 - beta) + Q[i] * beta);
     }
 }
 
@@ -57,7 +59,8 @@ static void shrink(double **s, int n, int ia)
 {
     for (int i = 1; i < n + 1; i++)
     {
-        if(i != ia){
+        if (i != ia)
+        {
             transform(s[i], s[ia], n, SHRINK, s[i]);
         }
     }
@@ -74,18 +77,152 @@ static int done(double **s, int n, double *y, int ia, int iz, double err2)
 {
     double euc_norm = 0.0;
     /*We are getting the euclidian norm of the best and worst functions*/
-    for(int j = 0; j < n; j++)
+    for (int j = 0; j < n; j++)
     {
         euc_norm += ((s[ia][j] - s[iz][j]) * (s[ia][j] - s[iz][j]));
     }
 
-    if(euc_norm < err2 && abs((y[iz] - y[ia])<= err2)){
+    if (euc_norm < err2 && abs((y[iz] - y[ia]) <= err2))
+    {
         return 1;
-    }else{
+    }
+    else
+    {
         return 0;
     }
 }
 
 int nelder_mead(struct nelder_mead *nm)
 {
-}
+    double **s = nm->s;
+    int n = nm->n;
+    double h = nm->h;
+    double tol = nm->tol;
+    double err2 = (h * tol) * (h * tol);
+    double *y, *C, *Pr, *Pe, *Pc;
+    double yr, ye, yc;
+    int ia, iy, iz;
+    int simplex_to_be_freed = 0;
+    int fevalcount;
+    int i, j;
+
+    make_vector(y, n + 1); // holds objective function values evaluated at the vertices
+    make_vector(Pr, n);    // pointer to the reflected vector vertex
+    make_vector(Pe, n);    // pointer to the expanded vector vertex
+    make_vector(Pc, n);    // pointer to the contracted vector vertex
+    make_vector(C, n);     // pointer to the centroid of the face opp the worst objective function vertex
+
+    if (s == NULL)
+    {
+        make_matrix(s, n + 1, n);
+        simplex_to_be_freed = 1;
+
+        // Adding entries to the matrix s
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                s[i][j] = nm->x[j];
+                if (i == j + 1)
+                {
+                    s[i][j] += h;
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        y[i] = nm->f(s[i], n, nm->params);
+    }
+    fevalcount = n + 1;
+
+    while (fevalcount <= nm->maxevals)
+    {
+        rank_vertices(y, n + 1, &ia, &iy, &iz);
+        if (done(s, n, y, ia, iz, err2))
+        {
+            nm->minval = y[ia];
+            // copy the best vertex into the vector nm → x ...
+            memcpy(nm->x, s[ia], n * sizeof(double));
+            break;
+        }
+        get_centroid(s, n, iz, C);
+        transform(C, s[iz], n, -REFLECT, Pr);
+        yr = nm->f(Pr, n, nm->params);
+        fevalcount++;
+
+        if (yr < y[ia])
+        {
+            // Case 1
+            transform(C, Pr, n, EXPAND, Pe);
+            ye = nm->f(Pe, n, nm->params);
+            fevalcount++;
+            if (ye < yr)
+            {
+                replace_row(s, iz, &Pe);
+                y[iz] = ye;
+            }
+            else
+            {
+                replace_row(s, iz, &Pr);
+                y[iz] = yr;
+            }
+        }
+        else if (yr < y[iy])
+        {
+            // Case 2
+            replace_row(s, iz, &Pr);
+            y[iz] = yr;
+        }
+        else
+        {
+            if (yr < y[iz])
+            {
+                // Outer contraction
+                transform(C, Pr, n, CONTRACT, Pc);
+                yc = nm->f(Pc, n, nm->params);
+                fevalcount++;
+
+                if (yc < yr) // Accept outer contraction
+                {
+                    replace_row(s, iz, &Pc);
+                    y[iz] = yc;
+                }
+                else // Otherwise shrink
+                {
+                    shrink(s, n, ia);
+                    y[iy] = nm->f(s[iy], n, nm->params);
+                    fevalcount++;
+                    y[iz] = nm->f(s[iz], n, nm->params);
+                    fevalcount++;
+                }
+            }
+            else
+            {
+                //inner contraction
+                transform(C, s[iz], n, CONTRACT, Pc);
+                yc = nm->f(Pc, n, nm->params);
+                fevalcount++;
+
+                if (yc < y[iz]) // Accept inner contraction
+                {
+                    replace_row(s, iz, &Pc);
+                    y[iz] = yc;
+                }
+                else // Otherwise shrink
+                {
+                    shrink(s, n, ia);
+                    y[iy] = nm->f(s[iy], n, nm->params);
+                    fevalcount++;
+                    y[iz] = nm->f(s[iz], n, nm->params);
+                    fevalcount++;
+                }
+            }
+        } // end of the while-loop
+
+        // free vectors y, C, Pr, Pe, Pc
+        if (simplex_to_be_freed)
+            free_matrix(s);
+        return fevalcount;
+    }
